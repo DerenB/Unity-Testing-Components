56bdf6a3c699d30f5e0c3b86094f79905cdc9fb1
Deren B <dbozerprogramming@gmail.com>
2022-03-21 03:03:52 -0400

    Farm testing

---
+++ Farm-Camera-Movement/Assets/Camera_One/PlayerCamera.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public Transform cameraPivot;
+    public Camera cameraObject;
+
+    // Object the camera will follow
+    public GameObject player;
+
+    Vector3 cameraFollowVelocity = Vector3.zero;
+    Vector3 targetPosition;
+
+    [Header("Camera Speeds")]
+    float cameraSmoothTime = 0.2f;
+
+    public void HandleAllCameraMovement()
+    {
+        // Follow the Player
+        FollowPlayer();
+
+        // Rotate the camera
+    }
+
+    private void FollowPlayer()
+    {
+        targetPosition = Vector3.SmoothDamp(transform.position, player.transform.position, ref cameraFollowVelocity, cameraSmoothTime * Time.deltaTime);
+        transform.position = targetPosition;
+    }
+
+}
