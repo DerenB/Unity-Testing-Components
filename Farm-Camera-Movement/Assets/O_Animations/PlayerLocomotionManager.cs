20f87140646469c5cd7f460bdbbe108575a8e668
Deren B <dbozerprogramming@gmail.com>
2022-03-21 21:38:54 -0400

    Camera Rotation

---
+++ Farm-Camera-Movement/Assets/O_Animations/PlayerLocomotionManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLocomotionManager : MonoBehaviour
+{
+    InputManager inputManager;
+
+    [Header("Camera Transform")]
+    public Transform playerCamera;
+
+    // How fast the character will rotate
+    [Header("Movement Speed")]
+    public float rotationSpeed = 3.5f;
+
+    [Header("Rotation Variables")]
+    Quaternion targetRotation; // The place we want to rotate
+    Quaternion playerRotation; // The current rotation
+
+    private void Awake()
+    {
+        inputManager = GetComponent<InputManager>();
+    }
+
+    public void HandleAllLocomotion()
+    {
+        // Handle Rotation
+        HandleRotation();
+
+        // Handle Falling
+    }
+
+    private void HandleRotation()
+    {
+        targetRotation = Quaternion.Euler(0, playerCamera.eulerAngles.y, 0);
+        playerRotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        if(inputManager.verticalMovementInput != 0 || inputManager.horizontalMovementInput != 0)
+        {
+            transform.rotation = playerRotation;
+        }
+    }
+}
