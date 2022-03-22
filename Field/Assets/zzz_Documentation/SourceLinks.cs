54d2850565f20dc5a6c7d906543ca3a0db667b4f
Deren B <dbozerprogramming@gmail.com>
2022-03-22 03:56:59 -0400

    Simple Camera

---
+++ Field/Assets/zzz_Documentation/SourceLinks.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SourceLinks : MonoBehaviour
+{
+
+
+    // Links for all the sources uses
+
+    // Farm Models:
+    // https://assetstore.unity.com/packages/3d/environments/industrial/polygon-farm-low-poly-3d-art-by-synty-146192
+
+    // Animations:
+    // https://www.mixamo.com/#/?page=1&query=&type=Motion%2CMotionPack
+
+    // Video on setting up animation settings for imported animations
+    // https://youtu.be/9H0aJhKSlEQ
+
+    // Video on setting up animation blend:
+    // https://youtu.be/6uxom-Db_So
+
+    // Simple Third Person camera video
+    // https://youtu.be/jPU2ri4ZwxM
+}
