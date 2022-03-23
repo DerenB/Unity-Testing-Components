30d8e88b95994cfbb37d7280c74d2e35486864d5
Deren B <dbozerprogramming@gmail.com>
2022-03-23 16:30:28 -0400

    Changing the character model and removing unneeded files

--- Field/Assets/zzz_Documentation/SourceLinks.cs
+++ Field/Assets/zzz_Documentation/SourceLinks.cs
@@ -8,11 +8,14 @@ public class SourceLinks : MonoBehaviour
 
     // Links for all the sources uses
 
-    // Farm Models:
-    // https://assetstore.unity.com/packages/3d/environments/industrial/polygon-farm-low-poly-3d-art-by-synty-146192
+    // Free Starter Models
+    // https://assetstore.unity.com/packages/3d/props/polygon-starter-pack-low-poly-3d-art-by-synty-156819
 
     // Animations:
     // https://www.mixamo.com/#/?page=1&query=&type=Motion%2CMotionPack
+
+    // Primary Source (playlist):
+    // https://youtube.com/playlist?list=PLD_vBJjpCwJsqpD8QRPNPMfVUpPFLVGg4
 
     // Video on setting up animation settings for imported animations
     // https://youtu.be/9H0aJhKSlEQ
